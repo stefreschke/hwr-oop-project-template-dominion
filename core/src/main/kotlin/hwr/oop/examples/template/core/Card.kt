bf6db9b6c638e3e2bb80fabbc41877d8de58bd56
henri <s_nemeth25@stud.hwr-berlin.de>
2026-05-15 15:04:42 +0200

    card interface

---
+++ core/src/main/kotlin/hwr/oop/examples/template/core/Card.kt
@@ -0,0 +1,12 @@
+package hwr.oop.examples.template.core
+
+interface Card {
+    val name: String
+    val cost: Int
+
+    val actions: Int
+    val purchases: Int
+
+    val money: Int
+    val points: Int
+}
