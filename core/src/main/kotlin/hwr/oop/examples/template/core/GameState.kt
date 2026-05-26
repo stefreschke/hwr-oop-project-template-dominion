7eff035ac4d2b15b6427ed1913799ed3ea8cacb0
henri <s_nemeth25@stud.hwr-berlin.de>
2026-05-26 09:17:44 +0200

    refactors some parts increasing readability

--- core/src/main/kotlin/hwr/oop/examples/template/core/GameState.kt
+++ core/src/main/kotlin/hwr/oop/examples/template/core/GameState.kt
@@ -15,18 +15,15 @@ data class GameState (private val id: Int,
 
         val startingDeck = List(7) { CardID.COPPER } + List(3) { CardID.ESTATE }
 
+        val playerA = Player("alpha", 0, Deck(0, startingDeck))
+        val playerB = Player("beta", 1, Deck(1, startingDeck))
+        val playerC = Player("gamma", 2, Deck(2, startingDeck))
+        val playerD = Player("delta", 3, Deck(3, startingDeck))
+
         val players: List<Player> = when(this.playersCount) {
-            2 -> listOf(Player("alpha", 0, Deck(0, startingDeck)), Player("beta", 1, Deck(1, startingDeck)))
-            3 -> listOf(Player("alpha", 0, Deck(0, startingDeck)), Player(
-                "beta",
-                1,
-                Deck(1, startingDeck)
-            ), Player("gamma", 2, Deck(2, startingDeck)))
-            4 -> listOf(Player("alpha", 0, Deck(0, startingDeck)), Player(
-                "beta",
-                1,
-                Deck(1, startingDeck)
-            ), Player("gamma", 2, Deck(2, startingDeck)), Player("delta", 3, Deck(3, startingDeck)))
+            2 -> listOf(playerA, playerB)
+            3 -> listOf(playerA, playerB, playerC)
+            4 -> listOf(playerA, playerB, playerD, playerD)
             else -> throw IllegalArgumentException("game only possible with 2-4 players")
         }
         return GameState(this.id, this.playersCount, players = players)
