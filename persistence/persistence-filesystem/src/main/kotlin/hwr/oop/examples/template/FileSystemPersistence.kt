85adcbeaad5210f94cc555c3a664117c62ba29b7
henri <s_nemeth25@stud.hwr-berlin.de>
2026-06-11 13:35:46 +0200

    began impl of fileSysPersistence

--- persistence/persistence-filesystem/src/main/kotlin/hwr/oop/examples/template/FileSystemPersistence.kt
+++ persistence/persistence-filesystem/src/main/kotlin/hwr/oop/examples/template/FileSystemPersistence.kt
@@ -1,21 +1,30 @@
 package hwr.oop.examples.template
 
 import hwr.oop.examples.template.core.DominionPersistence
+import hwr.oop.examples.template.core.Game
 import hwr.oop.examples.template.core.GameInstance
 import okio.FileSystem
 
 class FileSystemPersistence(
 	configuration: FileSystemPersistenceConfiguration,
 	private val fileSystem: FileSystem = FileSystem.SYSTEM,
+	private val games: Map<String, GameInstance>
 ) : DominionPersistence{
 
 	private val directory = configuration.directory
 
+
+
 	override fun load(gameId: String): GameInstance {
-		TODO("Not yet implemented")
+		 return games[gameId] ?: loadFromFile(gameId)
+	}
+
+	private fun loadFromFile(gameId: String): GameInstance {
+		TODO("missing")
 	}
 
 	override fun save(game: GameInstance) {
+		games.filterNot { it.value == game }
 		TODO("Not yet implemented")
 	}
 }
